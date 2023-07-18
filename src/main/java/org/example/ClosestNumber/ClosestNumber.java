974c68e76371fabf20f2fb0692bbf8f8b8a9d2d3
Buket Yurt <buketyurt01@gmail.com>
2023-07-18 21:21:56 +0300

    Find Closest numbers to target number

---
+++ src/main/java/org/example/ClosestNumber/ClosestNumber.java
@@ -0,0 +1,38 @@
+package org.example.ClosestNumber;
+
+import java.util.Scanner;
+
+public class ClosestNumber {
+    public static void main(String[] args) {
+            Scanner scanner =new Scanner(System.in);
+            System.out.println("Girilen Sayı : ");
+            int[] arr = { 15, 12, 788, 1, -1, -778, 2, 0 };
+            int targetNumber = scanner.nextInt();
+
+            int closestSmaller = findClosestSmaller(arr, targetNumber);
+            int closestLarger = findClosestLarger(arr, targetNumber);
+
+            System.out.println("Girilen sayıdan küçük en yakın sayı: " + closestSmaller);
+            System.out.println("Girilen sayıdan büyük en yakın sayı: " + closestLarger);
+        }
+
+        public static int findClosestSmaller(int[] arr, int target) {
+            int closest = Integer.MIN_VALUE;
+            for (int num : arr) {
+                if (num < target && num > closest) {
+                    closest = num;
+                }
+            }
+            return closest;
+        }
+
+        public static int findClosestLarger(int[] arr, int target) {
+            int closest = Integer.MAX_VALUE;
+            for (int num : arr) {
+                if (num > target && num < closest) {
+                    closest = num;
+                }
+            }
+            return closest;
+        }
+    }
