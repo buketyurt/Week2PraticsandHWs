e08fb3fa8c11e74a7df5b81ef57090e93a8fe02e
Buket Yurt <buketyurt01@gmail.com>
2023-07-19 23:38:58 +0300

    getter and setter

--- src/main/java/org/example/HW2/Phone.java
+++ src/main/java/org/example/HW2/Phone.java
@@ -6,7 +6,16 @@ public class Phone extends Product{
     public Phone(){
 
     }
-    public Phone(int id, String name,double price,String phoneNumber){
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Phone(int id, String name, double price, String phoneNumber){
         super(id,name,price);
 
         this.phoneNumber =phoneNumber;
--- src/main/java/org/example/HW2/SmartPhone.java
+++ src/main/java/org/example/HW2/SmartPhone.java
@@ -8,6 +8,31 @@ public class SmartPhone extends Phone{
     public SmartPhone(){ //no args constructor
 
     }
+
+    public int getStorage() {
+        return storage;
+    }
+
+    public void setStorage(int storage) {
+        this.storage = storage;
+    }
+
+    public int getRam() {
+        return ram;
+    }
+
+    public void setRam(int ram) {
+        this.ram = ram;
+    }
+
+    public boolean isCamera() {
+        return camera;
+    }
+
+    public void setCamera(boolean camera) {
+        this.camera = camera;
+    }
+
     public SmartPhone(int id, String name, double price, String phoneNumber, int storage, int ram, boolean camera) { //all args constructor
         super(id, name, price, phoneNumber);
         this.storage = storage;
